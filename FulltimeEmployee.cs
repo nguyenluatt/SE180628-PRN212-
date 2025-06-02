b83bc9249a6f9e8650726e1a8e1b4c6ee7da9cdc
nguyenluatt <162718520+nguyenluatt@users.noreply.github.com>
2025-06-02 21:39:23 +0700

    Add files via upload

---
+++ FulltimeEmployee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    public class FulltimeEmployee:Employee
+    {
+        public override double calSalary()
+        {
+            return base.calSalary() + 5000000;
+        }
+
+        public string Tuoi()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+
+
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+List<Employee> employees = new List<Employee>();
+FulltimeEmployee fe1=new FulltimeEmployee()
+{
+    ID=2;
+    IdCard=456
+    Name="Name 2"
+    Birthday=new DateTime(1920,34,35),
+}
+
+ ID=3;
+IdCard = 123
+    Name = "Name 3"
+    Birthday = new DateTime(1920, 34, 35),
+}
+
+     ID = 4;
+IdCard = 789
+    Name = "Name 4"
+    Birthday = new DateTime(1920, 34, 35),
+}
+    employees.Add(pe1);
+
+ID = 5;
+IdCard = 512
+    Name = "Name 2"
+    Birthday = new DateTime(1920, 34, 35),
+WorkingHour=3
+};
+    employees.Add(pe2);
+Console.OutputEncoding=Encodin
+2)
+    Console.WriteLine("xuat toan bo nhan su");
+//c1:
+
+employees.ForEach(e=>Console.WriteLine(e));
+
+// cau 3 R Loc ra cac nhan su chinh thuc
+// cachs 1:
+
+List<FulltimeEmployee> fe_list=employees.OfType<FulltimeEmployee>().ToList();
+Console.WriteLine("cau 3: R--> Loc ra cac nhan su chinh thuc:");
+foreach(FulltimeEmployee fulltimeEmployee in fe_list)
+    Console.WriteLine(fe);
+
+
+//Cau 4: tong luong nhan cgunh thuc
+
+    double fe_sum_salary = fe_list.Sum(e => e.calSalary());
+Console.WriteLine("cau 4: tong luong chinh thuc:");
+Console.WriteLine(fe_sum_salary);
+
+// cau5 tong luong nv thoi vu
+
+double pe_sum_salary = employees.OfType(ParttimeEmployee > ().Sum(employees => e.calSalary());
+Console.WriteLine("cau 5: tong luong thoi vu:");
+Console.WriteLine(pe_sum_salary);
